2ab5c9f832cdc2006081aeaaeafce0d033752d1e
Ushiyariv.Timur <uti2000@yandex.ru>
2023-02-08 23:53:08 +0300

    Added third task

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 3;
+int exponent = 10;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
